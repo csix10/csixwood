b749f24e5ce9c470a7284aa7607a04ce8f09e4f4
csiki <balintbence800@gmail.com>
2025-09-29 14:35:01 +0200

    javitasok

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ import app.arajanlat_keszito as araj
 import app.adatgyujto as adat
 
 def szabasjegyzek():
-    df = faj.BeolvasKiirat().csv_beolvasasa_databol("galeria.csv")
-    #df = faj.BeolvasKiirat().csv_beolvas_df()
+    #df = faj.BeolvasKiirat().csv_beolvasasa_databol("galeria.csv")
+    df = faj.BeolvasKiirat().csv_beolvas_df()
 
-    araj.Arajanlat(df).elkeszites()
+    araj.Arajanlat(df, "Adlovits", "László").elkeszites()
 
 def proba_utdij():
     hup = adat.Utdij_kalkulator("Balatonfenyves, Vörösmarty utca 135.").utdij_kalkulacio()
